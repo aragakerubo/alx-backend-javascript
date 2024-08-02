a5095389c8cdf7e7182213abc0f23b96381e2064
aragakerubo <aragakerubo@gmail.com>
2024-08-02 16:45:28 -0400

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -46,3 +46,32 @@ const printTeacher: printTeacherFunction = (
 ): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+// Write a Class named StudentClass:
+
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
