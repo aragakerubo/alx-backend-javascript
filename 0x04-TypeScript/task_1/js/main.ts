d47231a9979176bcac20d67f518c93f9ca5ac06a
aragakerubo <aragakerubo@gmail.com>
2024-08-02 16:37:07 -0400

    2. Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,16 @@ const teacher3: Teacher = {
 };
 
 // console.log(teacher3);
+
+// Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: "Jane",
+    lastName: "Smith",
+    location: "London",
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
