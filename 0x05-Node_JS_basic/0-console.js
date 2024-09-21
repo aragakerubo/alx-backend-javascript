0a97302c99d3663dcb79c3dc367a8a59504a517a
aragakerubo <aragakerubo@gmail.com>
2024-09-21 15:13:50 -0700

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+// 0. Executing basic javascript with Node JS
+
+/**
+ * Prints to STDOUT the message passed as argument
+ * @param {*} message - Message to display
+ */
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
