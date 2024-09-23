a470c845b78c8d933fbdee911bcbb53cb29d6ba8
aragakerubo <aragakerubo@gmail.com>
2024-09-22 19:11:49 -0700

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,29 @@
 // 5. Create a more complex HTTP server using Node's HTTP module
 
-const http = require("http");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    if (req.url === "/") {
-        res.write("Hello Holberton School!");
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+
+    countStudents(process.argv[2])
+      .then(() => {
         res.end();
-    }
-
-    if (req.url === "/students") {
-        res.write("This is the list of our students\n");
-
-        countStudents(process.argv[2])
-            .then((data) => {
-                res.end();
-            })
-            .catch((error) => {
-                res.end(error.message);
-            });
-    }
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  }
 });
 
 app.listen(port);
