928a96dec086acd4a3a7135e8a6ce0032dae32c8
aragakerubo <aragakerubo@gmail.com>
2024-09-23 12:43:21 -0700

    7. Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,10 +1,39 @@
 // 7. Create a more complex HTTP server using Express
 
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
 const port = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data
+        .trim()
+        .split('\n')
+        .slice(1)
+        .filter((line) => line.length > 0);
+
+      const fields = {};
+
+      for (const line of lines) {
+        const student = line.trim().split(',');
+        if (!fields[student[3]]) {
+          fields[student[3]] = [];
+        }
+        fields[student[3]].push(student[0]);
+      }
+
+      resolve({ length: lines.length, students: fields });
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -13,7 +42,17 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   res.write('This is the list of our students\n');
   countStudents(process.argv[2])
-    .then(() => {
+    .then((data) => {
+      res.write(`Number of students: ${data.length}\n`);
+      for (const field in data.students) {
+        if (field) {
+          res.write(
+            `Number of students in ${field}: ${
+              data.students[field].length
+            }. List: ${data.students[field].join(', ')}\n`,
+          );
+        }
+      }
       res.end();
     })
     .catch((error) => {
