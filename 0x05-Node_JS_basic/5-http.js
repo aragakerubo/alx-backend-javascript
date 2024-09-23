084e1cfcc7798c59de7b27c25b44018af2d17ddd
aragakerubo <aragakerubo@gmail.com>
2024-09-22 19:08:12 -0700

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,62 +1,31 @@
 // 5. Create a more complex HTTP server using Node's HTTP module
 
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const countStudents = require("./3-read_file_async");
 
-const students = process.argv[2];
+const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.statusCode(200);
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  } else if (req.url === '/students') {
-    fs.readFile(students, 'utf8', (err, data) => {
-      if (err) {
-        res.write('Cannot load the database');
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    if (req.url === "/") {
+        res.write("Hello Holberton School!");
         res.end();
-      } else {
-        res.write('This is the list of our students\n');
-        const lines = data.trim().split('\n').slice(1);
-        const fields = {};
-        lines.forEach((line) => {
-          const student = line.trim().split(',');
-          if (fields[student[3]]) {
-            fields[student[3]].students.push(student[0]);
-          } else {
-            fields[student[3]] = {
-              field: student[3],
-              students: [student[0]],
-            };
-          }
-        });
+    }
 
-        res.write(`Number of students: ${lines.length}\n`);
+    if (req.url === "/students") {
+        res.write("This is the list of our students\n");
 
-        for (const field in fields) {
-          if (Object.hasOwnProperty.call(fields, field)) {
-            const element = fields[field];
-            res.write(
-              `Number of students in ${element.field}: ${
-                element.students.length
-              }. List: ${element.students.join(', ')}`,
-            );
-
-            if (
-              field
-                            !== Object.keys(fields)[Object.keys(fields).length - 1]
-            ) {
-              res.write('\n');
-            }
-          }
-        }
-        res.end();
-      }
-    });
-  }
+        countStudents(process.argv[2])
+            .then((data) => {
+                res.end();
+            })
+            .catch((error) => {
+                res.end(error.message);
+            });
+    }
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
